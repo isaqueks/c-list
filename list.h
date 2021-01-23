b311f5bf327518f066f6b4b3e9a11a01400b0665
Isaque <37017117+isaqueks@users.noreply.github.com>
2021-01-23 14:25:16 -0300

    Update list.h

--- list.h
+++ list.h
@@ -15,7 +15,7 @@ typedef struct
     unsigned int allocatedLength;
 } List;
 
-#define get_list_realAddr(list, index) (void *)(&list->objArray + (index) * list->objectSize)
+#define get_list_realAddr(list, index) (void *)(&(list->objArray[0]) + ((index) * list->objectSize))
 #define list_get_value(list, index, T) *(T *)list_get(list, index)
 
 List *create_list(int objectSize, int length);
